0b74bc3f886c972d14fc45a940ce11b94b01711f
Bharat-Runwal <bharatrunwal@gmail.com>
2020-07-10 14:42:25 +0530

    cv2

--- environment/enviro.py
+++ environment/enviro.py
@@ -1,11 +1,11 @@
 import numpy as np
 from PIL import Image
-import cv2
+
 import matplotlib.pyplot as plt
 import pickle
 from matplotlib import style 
 import time 
-
+import cv2
 style.use("ggplot")
 
 #config env
