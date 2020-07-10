f98dbafff077dc306cd509831aa3123eed71ceb1
Bharat-Runwal <ee3180598@iitd.ac.in>
2020-07-10 19:06:24 +0530

    Final

--- DQN/dqn-1.py
+++ DQN/dqn-1.py
@@ -13,6 +13,7 @@ import random
 import os 
 
 
+
 DISCOUNT = 0.99
 REPLAY_MEMORY_SIZE = 50_000  # How many last steps to keep for model training
 MIN_REPLAY_MEMORY_SIZE = 1_000  # Minimum number of steps in a memory to start training
